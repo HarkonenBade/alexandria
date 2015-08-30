1db603ab318666ed05ff5246bda5264af7a361cf
Hark <github@harkonen.net>
2015-08-30 22:01:19 +0100

    Easier debug settings

--- launch.py
+++ launch.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 
 import alexandria
+import sys
 
 if __name__ == "__main__":
-    alexandria.app.run(host="0.0.0.0", debug=True, use_reloader=False)
+    debug = "--debug" in sys.argv
+    alexandria.app.run(host="0.0.0.0", debug=debug, use_reloader=False)
