ab11a50e49d12f2667292fba605e014a191d82e9
Hark <github@harkonen.net>
2015-08-30 17:25:55 +0100

    Added markdown support

--- alexandria/static/index.js
+++ alexandria/static/index.js
@@ -1,3 +1,14 @@
+var renderer = new marked.Renderer();
+
+renderer.image = function (href, title, text) {
+    return '<img class="img-responsive" src="' + href + '" alt="' + text + '" title="' + title + '" />';
+}
+
+
 jQuery(document).ready(function() {
     jQuery("time.timeago").timeago();
+
+    $(".quote-text").each(function(index, current){
+        current.innerHTML = marked(current.innerHTML, {renderer: renderer});
+    });
 });
